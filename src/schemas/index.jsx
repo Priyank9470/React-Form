b221693dede879cd43c093c5acd827f3e2723676
Priyank9470 <patelpriyank@gmail.com>
2023-08-03 18:49:00 +0530

    My Commit

--- src/schemas/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as myyup from "yup"
-
-export const validate = myyup.object({
-    name: myyup.string().min(2).max(20).required("Please Enter your name"),
-    email: myyup.string().email().required("Please Enter your email"),
-    password: myyup.string().min(6).required("Please Enter your password"),
-    c_password: myyup.string().oneOf([myyup.ref("password"), null], "Password must be same").required()
-})
