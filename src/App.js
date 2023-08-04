fccc5ecbe498589500653e76e423d8ce752ca643
Priyank9470 <patelpriyank@gmail.com>
2023-08-04 15:30:46 +0530

    My Third Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import MyCustomeForm from "./Component/MyCustomeForm";
 import RegistrationForm from "./Component/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
       <RegistrationForm />
-      {/* <MyCustomeForm /> */}
     </div>
   );
 }
